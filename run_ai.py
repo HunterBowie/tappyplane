d4b010acce4fb115f30fc65e939e5faf945884a4
Hunter Bowie (Student) <1269473@learn.vsb.bc.ca>
2022-06-06 10:01:55 -0700

    idk

--- run_ai.py
+++ run_ai.py
@@ -1,4 +1,4 @@
-import neat, math
+import neat, math, random
 import game, windowgui, neural_net
 
 def fitness_function(genomes, config):
@@ -8,26 +8,58 @@ def fitness_function(genomes, config):
         genome.fitness = 0
     timer = windowgui.Timer()
     timer.start()
-    plane_dict = {genome: plane for _,genome in genomes for plane in new_game.planes}
+    network_timer = windowgui.Timer()
+    network_timer.start()
+
+    active_genomes = {genome: plane for _,genome in genomes for plane in new_game.planes}
     new_game.running = True
     while new_game.running and game.window.running:
-        if timer.passed(.1):
-            for genome,_ in plane_dict.items():
-                genome.fitness += .1
-            timer.start()
+        
 
         dead_genomes = []
-        for genome, plane in plane_dict.items():
+        for genome, plane in active_genomes.items():
             if not plane in new_game.planes:
                 dead_genomes.append(genome)
     
         for genome in dead_genomes:
-            del plane_dict[genome]
-            print(len(plane_dict))
+            del active_genomes[genome]
+            print("removed")
 
-        for genome, plane in plane_dict.items():
-            network = neat.nn.FeedForwardNetwork.create(genome, config)
-            # nearest_rock = None
+        if timer.passed(.1):
+            for genome, plane in active_genomes.items():
+                genome.fitness += .1
+            timer.start()
+
+        if network_timer.passed(.5):
+            for genome, plane in active_genomes.items():
+                    network = neat.nn.FeedForwardNetwork.create(genome, config)
+                    
+                    plane_y = plane.get_real_pos()[1]
+                    output = network.activate([plane_y])
+                    if output[0] > 0.5:
+                        plane.boost()
+        print(len(active_genomes))
+
+        new_game.update()
+        
+        
+        
+
+    if not game.window.running:
+        quit()
+    if not new_game.running:
+        print()
+
+
+if __name__ == "__main__":
+    game.window.start()
+    population = neural_net.init()
+    winner = population.run(fitness_function)
+    print("success")
+    neural_net.save_genome(winner, "best")
+
+
+# nearest_rock = None
             # nearest_dist = None
             # for rock in new_game.rocks:
             #     dist = math.dist(rock.get_rect().center, plane.get_real_pos())
@@ -39,27 +71,3 @@ def fitness_function(genomes, config):
             #         nearest_dist = dist
             #         nearest_rock = rock 
             
-            plane_y = plane.get_real_pos()[1]
-            output = network.activate([plane_y])
-            if output[0] > 0:
-                plane.boost()
-
-
-        new_game.update()
-        
-        
-        
-
-    if not game.window.running:
-        quit()
-    if not new_game.running:
-        pass
-        #All birds died
-
-
-if __name__ == "__main__":
-    game.window.start()
-    population = neural_net.init()
-    winner = population.run(fitness_function)
-    print("success")
-    neural_net.save_genome(winner, "best")
